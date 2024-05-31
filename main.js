a2c1801197fee1827df83c129df66a6ec56a24b7
Lindsay Bean <lcbean@unc.edu>
2024-05-30 23:36:43 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let songs = new Array(
+    "option 1",
+    "option 2",
+    "option 3",
+    "option 4",
+    "option 5"
+);
+
+let first = new Array();
+let parent = new Array();
+let equal = new Array();
+let rec = new Array();
+let cmp1,cmp2; 
+
+function initList(){
+    let n = 0;
+    let mid; 
+    let i; 
+
+
+}
